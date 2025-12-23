c1d51e7538171de179a0fc3e29077331eaf7fda2
ADITYA KUMAR <adityak28939@gmail.com>
2025-12-24 01:53:16 +0530

    remove chapter3/index.py file

---
+++ chapter3/03_rules_variables.py
@@ -0,0 +1,13 @@
+a = 23
+
+aaa = 435
+
+aditya = 34
+
+sameer = 45
+
+_sameer = 34
+
+#@sameer = 56 # invalid due to @ symbol
+
+# s@meer # invalid due to @ symbol
---
+++ chapter3/04_opertors.py
@@ -0,0 +1,32 @@
+# Airthmatic operators 
+a = 34
+b = 4
+c = a+b
+print(c)
+
+# Assignment operators
+a = 4-2 # Assign 4-2 in A
+print(a)
+b = 6
+b += 3 # increment the value of b by 3 and then assign it to b
+print(b)
+
+# Comparision operators
+d =5<6
+print(d)
+
+# logical operators 
+e = True or False 
+
+# Truth table of 'or'
+print("True or Fase is", True or False )
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+#truth table of and 
+
+print("True and Fase is", True and False )
+print("True and True is", True and True)
+print("False and True is", False and True)
+print("False and False is", False and False)
